b21ea4ec084fcad2d4c85fae0ec2d407ff3b9a33
amandaanaju23 <125404603+amandaanaju23@users.noreply.github.com>
2023-04-25 15:18:08 -0300

    Add files via upload

---
+++ multiplicador.py
@@ -0,0 +1,7 @@
+print("Bem vindo ao Multiplicador")
+num1=int(input("digite um numero inteiro:"))
+num2=int(input("digite outro numero inteiro:"))
+x=int(num1)
+y=int(num2)
+resultado=x*y
+print(resultado)
