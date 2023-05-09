9eb160a0f3f17cfee8a12413baee8b4d489de764
amandaanaju23 <125404603+amandaanaju23@users.noreply.github.com>
2023-05-09 15:09:42 -0300

    Add files via upload

---
+++ letra.py
@@ -0,0 +1,13 @@
+char=input("digite uma letra")
+if char == "a":
+   print("voce digitou uma vogal")
+elif char =="e":
+   print("voce digitou uma vogal")
+elif char == "i":
+   print("voce digitou uma vogal")
+elif char == "o":
+   print ("voce digitou uma vogal")
+elif char == "u":
+   print ("voce digitou uma vogal")
+else:
+   print("voce digitou uma consoante") 
